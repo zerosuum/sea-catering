b5c71c615b836a1e32f2541179d4ce4edee1f11a
zerosuum <dellanurizki@Dellas-MacBook-Air.local>
2025-07-01 19:12:55 +0700

    inisiasi code

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // Setiap request dari frontend yang dimulai dengan '/api'
+      "/api": {
+        // Akan diteruskan ke server backend Anda
+        target: "http://127.0.0.1:5000",
+
+        // Ini penting agar backend tidak bingung dengan origin request
+        changeOrigin: true,
+      },
+    },
+  },
+});
